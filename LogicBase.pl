278cbb76efa2b59a5d75dcfde5d58b01e838ce68
Hugo Carneiro <hugofmealt@gmail.com>
2019-06-03 19:54:21 -0300

    desisti dos comentarios

--- LogicBase.pl
+++ LogicBase.pl
@@ -1,45 +1,41 @@
 %% Ok, a principio estarei listando aqui a base das relacoes do Henrique
-%% VII conforme a wikipedia
+%% VII conforme a wikipedia.
 %% (https://pt.wikipedia.org/wiki/Henrique_VIII_de_Inglaterra). Vou
 %% tentar fazer a principio so o grosso das relacoes familiares diretas
 %% dele que eu e thiago olhamos na quinta.
 %
 
-/** Relacoes familiares diretas e legitimas */
-
-/** Da king */
-
-pai(henriqueVII,henriqueVIII). /** Favor manter a forma pai(Pai,Filho) */
-mae(isabeldeIorque, henriqueVIII). /** Mesmo role */
-
-
-/** Da Kings Children */
-
-/** Catarina de Aragao */
 
 pai(henriqueVIII, henrique).
 pai(henriqueVIII, maria).
-pai(henriqueVIII, henrique).  /** Tiveram dois henriques */
+pai(henriqueVIII, henrique).
+pai(henriqueVIII, isabel).
+pai(henriqueVIII, henrique).
+pai(henriqueVIII, henrique).
+pai(henriqueVIII, eduardo).
+pai(henriqueVII,henriqueVIII).
+mae(isabeldeIorque, henriqueVIII).
+
 mae(catarinadeAragao, henrique).
 mae(catarinadeAragao, maria).
 mae(catarinadeAragao, henrique).
 
-/** Ana Bolena */
-pai(henriqueVIII, isabel).
-pai(henriqueVIII, henrique).
 mae(anaBolena, isabel).
 mae(anaBolena, henrique).
 
-/** Joana Seymour */
-
-pai(henriqueVIII, eduardo).
 mae(joanaSeymour, eduardo).
 
 
 
-/** Isabel Blount */
 
-pai(henriqueVIII, henrique).
+
+
+
+
+
+
+
+
 
 
 
